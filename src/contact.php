9d1bd992c98bffcb7eaec0d5aa73bee038b822b7
nesta.web <nesta.web.dev@gmail.com>
2023-02-04 12:42:28 +0100

    contact

--- src/contact.php
+++ src/contact.php
@@ -12,7 +12,7 @@
         <title>Nestaweb • Contact</title>
 
         <link rel='stylesheet' href='../css/default.css'>
-        <link rel='stylesheet' href='../css/appointment.css'>
+        <link rel='stylesheet' href='../css/contact.css'>
     </head>
     <body>
         <?php include_once "../components/transition.php" ?>
@@ -28,35 +28,17 @@
             <form action="../backend/sendMessage.php" method="post" id="getInTouch">
                 <div class="inputGetInTouch">
                     <label for="clientName">Votre nom</label>
-                    <input type="text" name="clientName" id="clientName">
+                    <input type="text" name="clientName" id="clientName" placeholder="John Doe">
                 </div>
                 <div class="inputGetInTouch">
                     <label for="clientEmail">Votre e-mail</label>
-                    <input type="text" name="clientEmail" id="clientEmail">
+                    <input type="email" name="clientEmail" id="clientEmail" placeholder="exemple@gmail.com">
                 </div>
                 <div class="inputGetInTouch">
-                    <label for="interest"></label>
-                    <select name="interest" id="interest">
-                        <option value="">Site vitrine</option>
-                        <option value="">Blog</option>
-                        <option value="">Petite boutique</option>
-                        <option value="">Marketplace</option>
-                        <option value="">CV et Personnel</option>
-                        <option value="">Portfolio</option>
-                        <option value="">Service</option>
-                        <option value="">Restaurant</option>
-                        <option value="">Entreprise local</option>
-                    </select>
+                    <label for="message">Votre message</label>
+                    <textarea name="message" id="message" placeholder="Parlez moi de votre projet"></textarea>
                 </div>
-                <div class="inputGetInTouch">
-                    <label for=""></label>
-                    <input type="text" name="" id="">
-                </div>
-                <div class="inputGetInTouch">
-                    <label for="message"></label>
-                    <textarea name="message" id="message"></textarea>
-                </div>
-                <button id="sendForm">Envoyer</button>
+                <button id="sendForm" class="cta secondary">Envoyer <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M7 40q-1.2 0-2.1-.9Q4 38.2 4 37V11q0-1.2.9-2.1Q5.8 8 7 8h34q1.2 0 2.1.9.9.9.9 2.1v26q0 1.2-.9 2.1-.9.9-2.1.9Zm17-15.1L7 13.75V37h34V13.75Zm0-3L40.8 11H7.25ZM7 13.75V11v26Z"/></svg></button>
             </form>
         </main>
         <footer>
